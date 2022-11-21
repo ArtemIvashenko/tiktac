eed3905ee66ffb6f06bf00a9b2401f01cc80043b
ArtemIvashenko <artemivashenko333@gmail.com>
2022-11-21 13:16:01 +0200

    add class

--- script.js
+++ script.js
@@ -15,17 +15,52 @@ function render(){
     ticTak.length = 9;
     move = 0;
 }
-function hod (){
-	
-	
+class PlayerX{
+	constructor(name){
+		this.name = name;
+	}
+	moveFirstPlayer(){
+	    player0move.hodTakMessage();
+                ticTak[event.target.dataset.id] = 'x';
+				event.target.innerHTML = resX;
+                event.target.classList.add('X');
+    }
+
+    winnerX(){
+	    $('.messageWinner').fadeIn(2000);
+        $('.messageWinner').fadeOut(2500);
+        $('.block').css({'opacity': '0.1'});
+    }
+    hodXMessage(){
+	    $('div').remove('.hodX');
+	    $('.pole').after(`<div class="hodX" > Ходят ${this.name}</div>`);
+    }
+}
+
+class Player0{
+	constructor(name){
+		this.name = name;
+	}
+	winnerNull(){
+	    $('.pole').append('<div class="messageWinnerNull"> win 0 </div>');
+	    $('.block').css({'opacity': '0.1'});s
+    }
+    hodTakMessage(){
+	    $('div').remove('.hodX');
+	    $('.pole').after(`<div class="hodX">Ходят ${this.name} </div>`);
+    }
+
+}
+
+function hod (){	
 	$('.pole').click(function (event) {
 		
 		if (event.target.className === 'block'){
 			if (move % 2 ==0){
-				moveFirstPlayer();
+				playerXmove.moveFirstPlayer();
 			}else{
 				
-				hodXMessage();
+				playerXmove.hodXMessage();
                 ticTak[event.target.dataset.id] = '0'
 				event.target.innerHTML = res0;
                 event.target.classList.add('0');	
@@ -37,76 +72,72 @@ console.log(ticTak);
 		
 	})
 }
-function moveFirstPlayer(){
-	hodTakMessage();
-                ticTak[event.target.dataset.id] = 'x';
-				event.target.innerHTML = resX;
-                event.target.classList.add('X');
-}
+
 
 function winner() {
 	let pole = $('.block');
 	if(ticTak[0] =='x' && ticTak[1]  =='x' && ticTak[2] =='x'){
-		winnerX();
+		playerXmove.winnerX();
 		setTimeout(render, 3000);
+		hodXMessage();
 	}
 	if(ticTak[3]=='x' && ticTak[4] =='x' && ticTak[5]=='x'){
-		winnerX();
+		playerXmove.winnerX();
 		setTimeout(render, 3000);
 	}
 	if(ticTak[6]=='x' && ticTak[7] =='x' && ticTak[8]=='x'){
-		winnerX();
+		playerXmove.winnerX();
 		setTimeout(render, 3000);
 	}
 	if(ticTak[0]=='x' && ticTak[3] =='x' && ticTak[6]=='x'){
-		winnerX();
+		playerXmove.winnerX();
 		setTimeout(render, 3000);
 	}
 	if(ticTak[1]=='x' && ticTak[4] =='x' && ticTak[7]=='x'){
-		winnerX();
+		playerXmove.winnerX();
 		setTimeout(render, 3000);
 	}
 	if(ticTak[2]=='x' && ticTak[5] =='x' && ticTak[8]=='x'){
-		winnerX();
+		playerXmove.winnerX();
 		setTimeout(render, 3000);
 	}
 	if(ticTak[0]=='x' && ticTak[4] =='x' && ticTak[8]=='x'){
-		winnerX();
+		playerXmove.winnerX();
 		setTimeout(render, 3000);
 	}if(ticTak[2]=='x' && ticTak[4] =='x' && ticTak[6]=='x'){
-		winnerX();
+		playerXmove.winnerX();
 		setTimeout(render, 3000);
 	}
 
 	if(ticTak[0]=='0' && ticTak[1] =='0' && ticTak[2]=='0'){
-		winnerNull();
+		player0move.winnerNull();
 		setTimeout(render, 3000);
 	}
 	if(ticTak[3]=='0' && ticTak[4] =='0' && ticTak[5]=='0'){
-		winnerNull();
+		player0move.winnerNull();
 		setTimeout(render, 3000);
 	}
 	if(ticTak[6]=='0' && ticTak[7] =='0' && ticTak[8]=='0'){
-		winnerNull();
+		player0move.winnerNull();
 		setTimeout(render, 3000);
 	}
 	if(ticTak[0]=='0' && ticTak[3] =='0' && ticTak[6]=='0'){
-		winnerNull();
+		player0move.winnerNull();
 		setTimeout(render, 3000);
 	}
 	if(ticTak[1]=='0' && ticTak[4] =='0' && ticTak[7]=='0'){
-		winnerNull();
+		player0move.winnerNull();
 		setTimeout(render, 3000);
 	}
 	if(ticTak[2]=='0' && ticTak[5] =='0' && ticTak[8]=='0'){
-		winnerNull();
+		player0move.winnerNull();
 		setTimeout(render, 3000);
 	}
 	if(ticTak[0]=='0' && ticTak[4] =='0' && ticTak[8]=='0'){
-		winnerNull();
+		player0move.winnerNull();
 		setTimeout(render, 3000);
 	}if(ticTak[2]=='0' && ticTak[4] =='0' && ticTak[6]=='0'){
-		winnerNull();
+		player0move.winnerNull();
 		setTimeout(render, 3000);
 	}
 	
@@ -116,35 +147,14 @@ function getRandomInt(max) {
 }
 
 
-function winnerX(){
-	$('.messageWinner').fadeIn(2000);
-    $('.messageWinner').fadeOut(2500);
-    $('.block').css({'opacity': '0.1'});
-}
-function winnerNull(){
-	$('.pole').append('<div class="messageWinnerNull"> win 0 </div>');
-	$('.block').css({'opacity': '0.1'});
-}
-
-function hodXMessage(){
-	$('div').remove('.hodX');
-	 $('.pole').after('<div class="hodX" >Ходят Х</div>');
-
-}
-
-function hodTakMessage(){
-	$('div').remove('.hodX');
-	 $('.pole').after('<div class="hodX">Ходят 0 </div>');
-}
-
 function compPlay(){
 	
 	$('.pole').click(function (event) {
 		if (event.target.className === 'block'){
-				moveFirstPlayer();
+				playerXmove.moveFirstPlayer();
 			    winner();
 				
-				hodXMessage();
+				playerXmove.hodXMessage();
 				setTimeout(algoritmX, 2000);
 					
 				  winner();
@@ -201,6 +211,8 @@ function algoritmX(){
 
 }
 
+let playerXmove = new PlayerX('X');
+let player0move = new Player0('0');
 
 
 
@@ -209,14 +221,14 @@ $(document).ready(function(){
 
     $('#reset').click(function(){
     	render();
-    	hodXMessage();
+    	playerXmove.hodXMessage();
     	document.location.reload();
 
     });
     $('#start').click(function(){
     	render();
         selectPlay();
-    	hodXMessage();
+    	playerXmove.hodXMessage();
         
     });
 
